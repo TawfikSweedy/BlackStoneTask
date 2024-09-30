b8ed8e6c404a349c954cba145dd4c3f815efbe51
Tawfiq <Tawfik.Sweedy@grandmstg.com>
2024-09-30 18:52:42 +0300

    Initial Commit

---
+++ BlackStoneTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackStoneTask
+//
+//  Created by sameh mohammed on 30/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
